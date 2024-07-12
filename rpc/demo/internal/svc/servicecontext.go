fbe48d7a51189044aec83bfc523cc0971a03441f
xingwei <ingwei@163.com>
2024-07-12 11:58:02 +0800

    first

---
+++ rpc/demo/internal/svc/servicecontext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "gozero/rpc/demo/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
